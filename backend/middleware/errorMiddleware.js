66b191c9bf3108e8a5fea2f430257be1bce57348
mustafaad12 <mustafa.ssi.msn121@gmail.com>
2022-04-03 14:54:46 +0300

    create middleware error handler

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,17 @@
+
+const notFound = () => {
+  const error = new Error(`not found ${req.originalUrl}`)
+  res.status(404)
+  next(error)
+}
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
+  res.json({
+    message: err.message,
+    stack: err.stack
+  })
+}
+
+export {notFound, errorHandler}
